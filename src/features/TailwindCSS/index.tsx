fdf422d6fb4a45e4ed34335e55a7b4f64852224f
Binh Pham Thanh (CNTT - CVCC.PTUD) <binhpt@SBSI.VN>
2026-05-22 16:34:42 +0700

    update

--- src/features/TailwindCSS/index.tsx
+++ src/features/TailwindCSS/index.tsx
@@ -1,16 +1,16 @@
 import React from "react";
 
-const index = () => {
+const TailwindCSS = () => {
   return (
-    <div className="container mx-auto">
+    <div className="container mx-auto h-125">
       <div className="grid grid-cols-1 gap-4 sm:grid-cols-2 lg:grid-cols-4">
-        <div className="bg-blue-500 p-4">Phần tử 1</div>
-        <div className="bg-green-500 p-4">Phần tử 2</div>
-        <div className="bg-red-500 p-4">Phần tử 3</div>
-        <div className="bg-yellow-500 p-4">Phần tử 4</div>
+        <div className="border p-4">Block 1</div>
+        <div className="border p-4">Block 2</div>
+        <div className="border p-4">Block 3</div>
+        <div className="border p-4">Block 4</div>
       </div>
     </div>
   );
 };
 
-export default index;
+export default TailwindCSS;
